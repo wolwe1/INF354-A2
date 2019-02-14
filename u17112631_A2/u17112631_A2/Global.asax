3c9294728c110ba20cc4636884e4f51fa399a8e1
Mr. JJ Goschen <u17112631@up.ac.za>
2019-02-14 10:19:35 +0200

    Initial push

---
+++ u17112631_A2/u17112631_A2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="u17112631_A2.MvcApplication" Language="C#" %>
---
+++ u17112631_A2/u17112631_A2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(u17112631_A2.Startup))]
+namespace u17112631_A2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
