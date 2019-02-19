a551fa051591857c1ba2b9923de30bb9d9e7050c
Mr. JJ Goschen <u17112631@up.ac.za>
2019-02-19 13:11:18 +0200

    Code part 2

--- u17112631_A2/u17112631_A2/Controllers/HomeController.cs
+++ u17112631_A2/u17112631_A2/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using u17112631_A2.ViewModels;
 
 namespace u17112631_A2.Controllers
 {
@@ -12,6 +13,24 @@ namespace u17112631_A2.Controllers
         {
             return View();
         }
+
+        public ActionResult Report()
+        {
+            Report newReport = new Report();
+            newReport.fromDate = new DateTime(2011, 1, 31);
+            newReport.toDate = new DateTime(2012, 1, 29);
+
+            return View(newReport);
+        }
+
+        [HttpPost]
+        public ActionResult Report(Report r)
+        {
+            // Do the report things
+
+            return View(newReport);
+        }
+
 
         public ActionResult About()
         {
---
+++ u17112631_A2/u17112631_A2/ViewModels/Report.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u17112631_A2.ViewModels
+{
+    public class Report
+    {
+        public DateTime fromDate { get; set; }
+        public DateTime toDate { get; set; }
+
+    }
+}
